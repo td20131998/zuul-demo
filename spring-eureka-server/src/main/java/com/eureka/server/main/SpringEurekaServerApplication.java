a8dc2ad609b543b4ef8d8d9e2e881b7eeb69a3d9
td20131998 <td20131998@gmail.com>
2020-10-31 14:30:06 +0700

    create branch dev

--- spring-eureka-server/src/main/java/com/eureka/server/main/SpringEurekaServerApplication.java
+++ spring-eureka-server/src/main/java/com/eureka/server/main/SpringEurekaServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringEurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEurekaServerApplication.class, args);
 	}
-
 }
