5328482677e3eab17ffc6f7b66033b5c8d9f111d
td20131998 <td20131998@gmail.com>
2020-10-31 14:34:41 +0700

    update eureka-server

--- spring-eureka-server/src/main/java/com/eureka/server/main/SpringEurekaServerApplication.java
+++ spring-eureka-server/src/main/java/com/eureka/server/main/SpringEurekaServerApplication.java
@@ -1,15 +1,20 @@
 package com.eureka.server.main;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringEurekaServerApplication {
+public class SpringEurekaServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEurekaServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("aaaa");
+	}
 }
